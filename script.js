98a8109ef6d123e574712cb677685b1d84d114e6
Ivan22032009 <isenkoivan93@gmail.com>
2025-02-28 18:17:13 +0200

    New cabinet

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const API_BASE_URL = 'https://schoolproject-1-kkzu.onrender.com';
     const authToken = localStorage.getItem('authToken');
+    const cabinetLink = document.querySelector('.cabinetLink');
+    const authButton = document.querySelector('.authButton');
     
     if (authToken) {
         fetch(`${API_BASE_URL}/api/user`, {
             headers: { 'Authorization': `Bearer ${authToken}` }
         }).then(response => {
             if (response.ok) {
-                document.querySelector('.userNav').style.display = 'inline-block';
-                document.querySelector('.authButton').textContent = 'Вийти';
+                // Відображаємо посилання на кабінет
+                if(cabinetLink) cabinetLink.style.display = 'inline-block';
+                if(authButton) authButton.textContent = 'Вийти';
             }
         });
     }
 
-    const authButton = document.querySelector('.authButton');
     if (authButton) {
         authButton.addEventListener('click', () => {
             localStorage.removeItem('authToken');
-            window.location.reload();
+            window.location.href = 'index.html';
         });
     }
 });
