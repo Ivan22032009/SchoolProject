70d7822d55fc96358f3a0ff7e8b482a62bd01757
Ivan22032009 <isenkoivan93@gmail.com>
2025-03-03 20:51:46 +0200

    dontev

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
